8adadfe4452089366b2ab6fd7d323692fa1b0eee
Guilherme Melos <guilherme.melos@ufv.br>
2019-09-17 13:44:32 -0300

    arrumado

--- matrix.c
+++ matrix.c
@@ -1,24 +1,13 @@
 #include "matrix.h"
 
 void initMatrix(int rows, int cols, Celula *init){
-  int i;
-  struct no *atual = (PCelula) malloc(sizeof(Celula));
-  atual = init->right;
+  int i, j;
+  PCelula atual* = (PCelula) malloc(sizeof(Celula));
+  atual = init->below;
   //preencho linhas-cabeça
-  for(i = 0; i<rows; i++){
+  for(i = 0; i<rows; ++i){
     if(i!=rows-1){
       atual-> below = (PCelula) malloc(sizeof(Celula));
-      atual ->right = atual;
-
-  PCelula atual;
-  atual = (PCelula) malloc(sizeof(Celula));
-  atual= init->right;
-
-  //preencho linhas-cabeça
-  for(i = 0; i<rows; i++){
-    if(i!=rows-1){
-      atual-> right = (PCelula) malloc(sizeof(Celula));
-
       atual->i = -1;
       atual = atual->below;
     }
@@ -28,20 +17,13 @@ void initMatrix(int rows, int cols, Celula *init){
     }
   }
 
-  atual = init;
+  atual = init->right;
   //preencho colunas-cabeça
-  for(i = 0; i<cols; i++){
+  for(i = 0; i<cols; ++i){
     if(i!=cols-1){
-      atual-> right= (PCelula) malloc(sizeof(Celula));
-
-  atual= init->below;
-
-  //preencho colunas-cabeça
-  for(i = 0; i<cols; i++){
-    if(i!=cols-1){
-      atual-> below = (PCelula) malloc(sizeof(Celula));
+      atual-> right = (PCelula) malloc(sizeof(Celula));
       atual->j = -1;
-      atual -> below = atual;
+      atual->below = atual;
       atual = atual->right;
     }
     else{
