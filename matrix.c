a837ccaeaef8e0c329e4bf8388c1d65cbdce7118
Guilherme Melos <guilherme.melos@ufv.br>
2019-09-17 12:36:23 -0300

    hmmmm

--- matrix.c
+++ matrix.c
@@ -7,7 +7,7 @@ void initMatrix(int rows, int cols, Celula *init){
   //preencho linhas-cabeça
   for(i = 0; i<rows; i++){
     if(i!=rows-1){
-      atual-> below = (struct no*) malloc(sizeof(Celula));
+      atual-> below = (PCelula) malloc(sizeof(Celula));
       atual ->right = atual;
       atual->i = -1;
       atual = atual->below;
@@ -22,7 +22,7 @@ void initMatrix(int rows, int cols, Celula *init){
   //preencho colunas-cabeça
   for(i = 0; i<cols; i++){
     if(i!=cols-1){
-      atual-> right= (struct no*) malloc(sizeof(Celula));
+      atual-> right= (PCelula) malloc(sizeof(Celula));
       atual->j = -1;
       atual -> below = atual;
       atual = atual->right;
