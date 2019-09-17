352c158c2aac38f085349990f1340ef37b4996b2
Guilherme Melos <guilherme.melos@ufv.br>
2019-09-17 09:16:51 -0300

    aaa

--- matrix.h
+++ matrix.h
@@ -18,34 +18,8 @@ typedef struct no{
 }Celula;
 
 //iniciar todas as células cabeça
-void initMatrix(int rows, int cols, Celula *init){
-  int i, j;
-  struct no temp*;
-  temp = init->right;
-  //preencho linhas-cabeça
-  for(i = 0; i<rows; i++){
-    if(i!=rows-1){
-      temp-> right = (Apontador) malloc(sizeof(Celula));
-      temp->i = -1;
-      temp = temp->right;
-    }
-    else{
-      temp->right = init;
-    }
-  }
+void initMatrix(int rows, int cols, Celula *init);
 
-  temp = init->below;
-  //preencho colunas-cabeça
-  for(i = 0; i<cols; i++){
-    if(i!=cols-1){
-      temp-> below = (Apontador) malloc(sizeof(Celula));
-      temp->j = -1;
-      temp = temp->below;
-    }
-    else{
-      temp->below = init;
-  }
-}
 
 //struct para matriz esparsa(sujeito a alterações)
 typedef struct{
